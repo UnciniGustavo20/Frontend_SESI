0f82050b4725a1e6a0f50eb2bed528ccd28dc70a
Uncinigustavo20 <uncinigustavo149@gmail.com>
2026-05-15 12:14:03 -0300

    continuação frotend js

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 function executarSistema() {
-    const nome = document.getElementById("inputnome").value;
+    // dados de entrada 
+    const nome = document.getElementById("inputNome").value;
+    const idade = parseInt(document.getElementById("inputIdade").value);
+    const valor = parseFloat(document.getElementById("inputValor").value);
+    const cupom = document.getElementById("inputCupom").value === "true"
+
+    // dados de saída 
+    const msg = document.getElementById("mensagem-autorização");
+    const lista = document.getElementById("lista-estoque");
+    const relatorio = document.getElementById("relatorio-final");
+
+    // validação para cmapos vazios 
+    if (!nome || isNaN(idade) || isNaN(valor)) {
+        alert("Por favor, peencha todos os campos!!");
+        return;          
+    }
+
+    //regra de negócios 
+    if (idade >= 16){
+        msg.innerText = `venda autorizada ${nome}`;
+        msg.style.color = "#00ff88";
+
+    //desconto
+    let valorFinal = (valor > 500 || cupom) ? valor * 0.85 : valor;
+
+    // estoque
+    let estoque = ["placa de video", "processador", "memoria RAM"];
+    lista.innerHTML = ""; // limpa a lista anterior
+    
+    }
 
 
-}
+}       
