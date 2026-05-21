b7b8bf76eca7e713e3a671ee3b8bddcc55368dbf
UnciniGustavo20 <uncinigustavo149@gmail.com>
2026-05-21 12:11:44 -0300

    formulario de venda - versão 01

--- script.js
+++ script.js
@@ -6,28 +6,49 @@ function executarSistema() {
     const cupom = document.getElementById("inputCupom").value === "true"
 
     // dados de saída 
-    const msg = document.getElementById("mensagem-autorização");
+    const msg = document.getElementById("mensagem-autorizacao");
     const lista = document.getElementById("lista-estoque");
     const relatorio = document.getElementById("relatorio-final");
 
     // validação para cmapos vazios 
     if (!nome || isNaN(idade) || isNaN(valor)) {
         alert("Por favor, peencha todos os campos!!");
-        return;          
+        return;
     }
 
     //regra de negócios 
-    if (idade >= 16){
-        msg.innerText = `venda autorizada ${nome}`;
+    if (idade >= 16) {
+        msg.innerText = `Venda autorizada: ${nome}`;
         msg.style.color = "#00ff88";
 
-    //desconto
-    let valorFinal = (valor > 500 || cupom) ? valor * 0.85 : valor;
+        //desconto
+        let valorFinal = (valor > 500 || cupom) ? valor * 0.85 : valor;
 
-    // estoque
-    let estoque = ["placa de video", "processador", "memoria RAM"];
-    lista.innerHTML = ""; // limpa a lista anterior
-    
+        // estoque
+        let estoque = ["placa de video", "processador", "memoria RAM"];
+        lista.innerHTML = ""; // limpa a lista anterior
+
+        // forEach:percorre um array e aplica uma ação para cada elemento
+        estoque.forEach(item => {
+            let li = document.createElement("li");
+            li.innerText = `Item ${item} reservado.`;
+            lista.appendChild(li); // usado para adicionar um novo elemento ou texto
+        });
+
+        //reatorio
+        relatorio.style.display = "block";
+        relatorio.innerHTML = `
+        <strong> RESUMO DO PEDIDO <\strong><br>
+        cliente: ${nome}<br>
+        Total Original: R$ ${valor.toFixed(2)} <br>
+        <strong> Total com descoto: R$ ${valorFinal.toFixed(2)} <\strong>
+        `;
+    } else {
+        msg.innerText = "Venda bloqueada: menor de 16 anos.";
+        msg.style.color = "#f0f4444";
+        relatorio.style.display = "none";
+        lista.innerHTML = "";
+
     }
 
 
